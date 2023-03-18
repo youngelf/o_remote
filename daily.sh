75c07a46ca1bddb758abdb9551564d157372e14e
Vikram Aggarwal <vikram@eggwall.com>
2023-03-18 13:08:54 -0700

    fix linking

--- daily.sh
+++ daily.sh
@@ -5,8 +5,15 @@
 # And also run a bigger test for half an hour, in background
 stress --cpu 2 --timeout 1800 &
 
+
+
+
+
+############################ LAST rule
+# This must be in the end.
+
 # Sync, and update this file itself.
 cd /home/ubuntu && git clone https://github.com/youngelf/o_remote.git
-for i in  /home/ubuntu/o_remote; do
+for i in  /home/ubuntu/o_remote/*; do
     ln -s $i .
 done
