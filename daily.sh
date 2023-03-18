598229f05ea1c3f1cc41149154247e6708adef44
Vikram Aggarwal <vikram@eggwall.com>
2023-03-18 10:31:28 -0700

    new file

---
+++ daily.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Stuff to be run daily.
+
+# Run stress test for a minute
+stress --cpu=2 --timeout=60
+
+
+# Sync, and update this file itself.
+cd /home/ubuntu && git clone https://github.com/youngelf/o_remote.git
+for i in  /home/ubuntu/o_remote; do
+    ln -s $i .
+done
