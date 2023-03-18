49d3f36fa1f0dcc3a70803cd47d0a024f28350a0
Vikram Aggarwal <vikram@eggwall.com>
2023-03-18 13:04:49 -0700

    testing which machines get this

--- daily.sh
+++ daily.sh
@@ -2,11 +2,8 @@
 
 # Stuff to be run daily.
 
-# Run stress test for a minute
-stress --cpu 2 --timeout 60
-
-# And also run a bigger test for half an hour
-stress --cpu 2 --timeout 1800
+# And also run a bigger test for half an hour, in background
+stress --cpu 2 --timeout 1800 &
 
 # Sync, and update this file itself.
 cd /home/ubuntu && git clone https://github.com/youngelf/o_remote.git
